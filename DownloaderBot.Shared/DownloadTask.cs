1347af237c8414561eed9ea369874449a23b385d
fractall8 <122642797+fractall8@users.noreply.github.com>
2026-01-04 18:46:17 +0200

    Initial commit

---
+++ DownloaderBot.Shared/DownloadTask.cs
@@ -0,0 +1,10 @@
+﻿namespace DownloaderBot.Shared;
+
+public class DownloadTask
+{
+    public long ChatId  { get; set; }
+    
+    public int MessageId { get; set; }
+    
+    public string Url { get; set; } = string.Empty;
+}
