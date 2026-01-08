dbb88b153285164f6a13d2519f019fead6f48870
fractall8 <122642797+fractall8@users.noreply.github.com>
2026-01-08 16:16:38 +0200

    feat: add welcome bot response messages
    - add new service BotResponseService to deal with response messages

---
+++ DownloaderBot.Api/Services/IBotResponseService.cs
@@ -0,0 +1,12 @@
+﻿using Telegram.Bot.Types;
+
+namespace DownloaderBot.Api.Services;
+
+public interface IBotResponseService
+{
+    Task SendPrivateWelcomeAsync(long chatId, string userName);
+
+    Task SendGroupWelcomeAsync(long chatId, User[] newChatMembers);
+
+    Task<Message> SendQueuedMessageAsync(long chatId, int replyToMessageId);
+}
