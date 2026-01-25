581ed4830122aa306783b617c519ee610f674d43
fractall8 <122642797+fractall8@users.noreply.github.com>
2026-01-25 13:15:06 +0200

    feat: add cookies support to bypass yt block

--- DownloaderBot.Worker/Services/YtDlpDownloaderService.cs
+++ DownloaderBot.Worker/Services/YtDlpDownloaderService.cs
@@ -8,26 +8,35 @@ namespace DownloaderBot.Worker.Services;
 
 public class YtDlpDownloaderService(ILogger<YtDlpDownloaderService> logger) : IDownloaderService
 {
+    private readonly string cookiesPath = "/app/cookies.txt";
+    private readonly string userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
+
     private readonly YoutubeDL youtubeDL = new()
     {
-        YoutubeDLPath = "yt-dlp",
-        FFmpegPath = "usr/bin/ffmpeg",
-        OutputFolder = "app/downloads",
+        YoutubeDLPath = "/usr/local/bin/yt-dlp",
+        FFmpegPath = "/usr/bin/ffmpeg",
+        OutputFolder = "/app/downloads",
     };
 
+    [Obsolete]
     public async Task<VideoInfo> GetVideoInfoAsync(string url)
     {
-        var res = await youtubeDL.RunVideoDataFetch(url);
+        var options = GetOptions();
+
+        var res = await youtubeDL.RunVideoDataFetch(url, overrideOptions: options);
         if (!res.Success)
         {
-            throw new Exception("Failed to fetch info");
+            var errorMsg = string.Join('\n', res.ErrorOutput);
+
+            logger.LogError("Failed to fetch info: {Msg}", errorMsg);
+            throw new Exception($"Failed to fetch info: {errorMsg}");
         }
 
         var data = res.Data;
 
         long? size = null;
 
-        if (data.Formats != null && data.Formats.Length > 0)
+        if (data.Formats is { Length: > 0 })
         {
             size = data.Formats
                 .Where(f => f.FileSize > 0)
@@ -46,33 +55,36 @@ public class YtDlpDownloaderService(ILogger<YtDlpDownloaderService> logger) : ID
         };
     }
 
+    [Obsolete]
     public async Task<DownloadResult> DownloadAsync(string url)
     {
         var fileName = $"{Guid.NewGuid()}.mp3";
         var outputPath = Path.Combine("app/downloads", fileName);
 
-        var videoDataResult = await youtubeDL.RunVideoDataFetch(url);
+        var options = GetOptions();
+        var videoDataResult = await youtubeDL.RunVideoDataFetch(url, overrideOptions: options);
 
         if (!videoDataResult.Success)
         {
-            throw new Exception($"Failed to fetch metadata: {string.Join('\n', videoDataResult.ErrorOutput)}");
+            throw new Exception($"Failed to fetch metadata. Check logs for OAuth code! Error: {string.Join('\n', videoDataResult.ErrorOutput)}");
         }
 
         var title = videoDataResult.Data.Title ?? "Unknown Track";
         var cleanTitle = string.Join("_", title.Split(Path.GetInvalidFileNameChars()));
 
-        var options = new OptionSet
-        {
-            AudioFormat = AudioConversionFormat.Mp3,
-            ExtractAudio = true,
-            Output = outputPath,
-            EmbedThumbnail = true,
-            EmbedMetadata = true,
-            // ForceIPv6 = true,
-        };
+        var downloadOptions = GetOptions();
+        downloadOptions.AudioFormat = AudioConversionFormat.Mp3;
+        downloadOptions.ExtractAudio = true;
+        downloadOptions.Output = outputPath;
+        downloadOptions.EmbedThumbnail = true;
+        downloadOptions.EmbedMetadata = true;
 
         logger.LogInformation("Start downloading: {Url}", url);
-        var result = await youtubeDL.RunAudioDownload(url, AudioConversionFormat.Mp3, ct: CancellationToken.None, overrideOptions: options);
+        var result = await youtubeDL.RunAudioDownload(
+            url,
+            AudioConversionFormat.Mp3,
+            ct: CancellationToken.None,
+            overrideOptions: downloadOptions);
 
         if (!result.Success)
         {
@@ -97,5 +109,28 @@ public class YtDlpDownloaderService(ILogger<YtDlpDownloaderService> logger) : ID
 
         logger.LogInformation("Download completed: {Path}", finalPath);
         return new DownloadResult(finalPath, cleanTitle);
+    }
+
+    [Obsolete]
+    private OptionSet GetOptions()
+    {
+        var options = new OptionSet
+        {
+            NoCheckCertificates = true,
+        };
+
+        options.AddCustomOption("--add-header", $"User-Agent:{userAgent}");
+        options.AddCustomOption("--remote-components", "ejs:github");
+
+        if (File.Exists(cookiesPath))
+        {
+            options.Cookies = cookiesPath;
+        }
+        else
+        {
+            logger.LogWarning("Cookies file NOT FOUND at {Path}. Download will likely fail.", cookiesPath);
+        }
+
+        return options;
     }
 }
